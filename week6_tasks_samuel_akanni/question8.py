292f291b7ab7490187c969e5905506e6c858a8fb
Samuel Akanni <samuelakanni97@gmail.com>
2024-07-20 18:34:47 +0100

    removed f function

--- week6_tasks_samuel_akanni/question8.py
+++ week6_tasks_samuel_akanni/question8.py
@@ -1,3 +1,3 @@
 #Write a program to print the sum of the first 10 even numbers in Python
 even_number = sum(range(2, 31, 2))
-print(f"The sum of the first 10 even numbers is"" " + str(even_number))
+print("The sum of the first 10 even numbers is"" " + str(even_number))
