b2ef82b989f36f3857c16c3b3de92f99ad4cd14f
Samuel Akanni <samuelakanni97@gmail.com>
2024-08-27 14:10:45 +0100

    Updated question5 and question6 with improvements

--- week6_tasks_samuel_akanni/question5.py
+++ week6_tasks_samuel_akanni/question5.py
@@ -1,7 +1,6 @@
 #Write a function that accepts the age of 4 people and displays the oldest
 def find_oldest(age):
     oldest = max(age)
-    print(f"The oldest age is {oldest}")
-age = [int(input(f"Enter age of person {i+1}: ")) 
-        for i in range(4)]
-find_oldest(age) 
+    print("The oldest age is " + str(oldest))
+age = [int(input("Enter age of person {}: ".format(i + 1))) for i in range(4)]
+find_oldest(age)
--- week6_tasks_samuel_akanni/question6.py
+++ week6_tasks_samuel_akanni/question6.py
@@ -1,8 +1,7 @@
 #Write a function that accepts the age of 4 people and displays the youngest.
 def find_youngest(age):
     youngest = min(age)
-    print(f"The youngest age is {youngest}")
+    print("The youngest age is " + str(youngest))
 
-# Accept ages of 4 people
-age = [int(input(f"Enter age of person {i+1}: ")) for i in range(4)]
+age = [int(input("Enter age of person {}: ".format(i + 1))) for i in range(4)]
 find_youngest(age)
